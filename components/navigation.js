7afad6f584966ebcd36aab2227b8faf32c5182d2
Haron Paulson <haron.paulson@anitechgroup.com>
2023-04-25 11:13:16 +0530

    first

---
+++ components/navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navigation () {
+    return(
+        <div>
+            
+            <h1 className=" font-semibold mb-2 text-2xl pl-10 ">Cost Estimate for audiometric assessment</h1>
+        </div>
+    )
+}
+
+export default Navigation;
